8d1909bbc64b1479b766bea4d11a5b44d57a253e
epec254 <epec254>
2024-07-28 21:09:06 +0000

    Agent initial commit

---
+++ agent_app_sample_code/agent_with_retriever/06_iterate_on_code_and_config.py
@@ -0,0 +1,13 @@
+# Databricks notebook source
+# TODO: Add this notebook showing how to do this step:
+# 1. Adjust code/config
+# 2. Log model
+# 3. Run evaluation
+
+
+
+# COMMAND ----------
+
+# MAGIC %environment
+# MAGIC "client": "1"
+# MAGIC "base_environment": ""
